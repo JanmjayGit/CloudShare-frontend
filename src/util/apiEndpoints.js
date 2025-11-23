1ec69394cb16ae4f2d870642024d666b6e478b20
Janmjay <janmjay@Janmjays-MacBook-Air.local>
2025-11-23 16:28:42 +0530

    url fixed

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:8080/api/v1.0';
+// const BASE_URL = 'http://localhost:8080/api/v1.0';
+const BASE_URL = 'https://cloudshare-backend-e8lk.onrender.com/api/v1.0';
 
 const apiEndpoints = {
     FETCH_FILES: `${BASE_URL}/files/my`,
