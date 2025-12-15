3628d4f9910d5a44a88f5e0dab62e5419543d9c5
Janmjay <janmjay@Janmjays-MacBook-Air.local>
2025-12-15 11:38:01 +0530

    vercel.json

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'http://localhost:8080/api/v1.0';
-const BASE_URL = 'https://cloudshare-backend-e8lk.onrender.com/api/v1.0';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const apiEndpoints = {
     FETCH_FILES: `${BASE_URL}/files/my`,
